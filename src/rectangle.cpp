be885c6cbc0740955fe1bf88cbccbf80cbc063d9
Jackie-Qin <jackieqin221@gmail.com>
2020-11-07 20:55:13 -0800

    testing failing tests

--- src/rectangle.cpp
+++ src/rectangle.cpp
@@ -6,10 +6,17 @@ class Rectangle {
 	int length;
 	int width;
         Rectangle(int l, int w) {
+		//if(l < w){
+		//    temp = w;
+		//    w = l;
+		//    l = temp;
+		//}
 		length=l; width=w;
-		if(length<0){length=0-length;}
-		if(width<0){width=0-width;}
+		//if(length<0){length=0-length;}
+		//if(width<0){width=0-width;}
 	};
+	int length() { return length;}
+	int width() { return width;}
         int area(){return length*width;}
 	int perimeter(){return (2*length)+(2*width);}
 };
--- tests/test.cpp
+++ tests/test.cpp
@@ -5,14 +5,61 @@
 
 #include "src/rectangle.cpp"
 
-TEST(Example, RectangleArea) {
+TEST(ConstructorTest, Rectangle) {
+    Rectangle* test = new Rectangle(10,5);
+    EXPECT_EQ(test->width(), 5);
+}
+
+TEST(ConstructorTest, Rectangle) {
+    Rectangle* test = new Rectangle(5,10);
+    EXPECT_EQ(test->length(), 10);
+}
+
+TEST(ConstructorTest, Rectangle) {
+    Rectangle* test = new Rectangle(-10,5);
+    EXPECT_EQ(test->length(), 10);
+}
+
+TEST(AreaTest, RectangleArea) {
     Rectangle* test = new Rectangle(10,10);
     EXPECT_EQ(test->area(), 100);
 }
 
-TEST(Example, RectanglePerimeter) {
+TEST(AreaTest, RectangleArea) {
+    Rectangle* test = new Rectangle(-1,10);
+    EXPECT_EQ(test->area(), 10);
+}
+
+TEST(AreaTest, RectangleArea) {
+    Rectangle* test = new Rectangle(10,0);
+    EXPECT_EQ(test->area(), 0);
+}
+
+TEST(AreaTest, RectangleArea) {
+    Rectangle* test = new Rectangle(-5,-7);
+    EXPECT_EQ(test->area(), 35);
+}
+
+TEST(PeriTest, RectanglePerimeter) {
     Rectangle* test = new Rectangle(15, 17);
     EXPECT_EQ(test->perimeter(), 64);
 }
+
+TEST(PeriTest, RectanglePerimeter) {
+    Rectangle* test = new Rectangle(12, 7);
+    EXPECT_EQ(test->perimeter(), 38);
+}
+
+TEST(PeriTest, RectanglePerimeter) {
+    Rectangle* test = new Rectangle(0, 10);
+    EXPECT_EQ(test->perimeter(), 20);
+}
+
+TEST(PeriTest, RectanglePerimeter) {
+    Rectangle* test = new Rectangle(-5, 10);
+    EXPECT_EQ(test->perimeter(), 30);
+}
+
+
 
 #endif //__TEST_CPP__
